b184431a94b3b157c08b7045f9728163cb518480
Guillem <al435545@uji.es>
2025-10-06 11:07:35 +0200

    HEcha tarea de modelaje (Task 1)

--- Teleoperation/Worlds/scripts/publish_transform.bash
+++ Teleoperation/Worlds/scripts/publish_transform.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
 export ROS_LOCALHOST_ONLY=1
-ros2 run tf2_ros static_transform_publisher --x 1 --y 8 --z 0 \
+ros2 run tf2_ros static_transform_publisher --x -4.2 --y 8.2 --z 0 \
   --qx 0 --qy 0 --qz 0 --qw 1 --frame-id map --child-frame-id odom
--- Teleoperation/scripts/launch_rosbag.bash
+++ Teleoperation/scripts/launch_rosbag.bash
@@ -2,4 +2,4 @@
 source /opt/ros/humble/setup.bash
 export ROS_LOCALHOST_ONLY=1
 export TURTLEBOT3_MODEL=burger
-ros2 bag play rosbag2_2025_09_17-15_58_59
+ros2 bag play ROSBAG_simulacion_modelo_lab
