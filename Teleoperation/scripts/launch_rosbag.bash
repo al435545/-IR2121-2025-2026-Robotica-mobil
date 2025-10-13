b74205dd97a6a1799e9b3b3f90728efdee623b4e
al435545 <al435545@uji.es>
2025-10-13 18:31:48 +0200

    .

--- Teleoperation/scripts/launch_rosbag.bash
+++ Teleoperation/scripts/launch_rosbag.bash
@@ -2,4 +2,6 @@
 source /opt/ros/humble/setup.bash
 export ROS_LOCALHOST_ONLY=1
 export TURTLEBOT3_MODEL=burger
-ros2 bag play ROSBAG_simulacion_modelo_lab
+ros2 bag play ROSBAG_AUTOCALIBRADO_EJ_1 /clock /map /odom /scan /tf /tf_static 
+	/amcl_pose /particle_cloud /robot_description
+
