7aa209a8cdd32b1a01653c39276ad0111f0a3649
al435545 <al435545@uji.es>
2025-11-19 18:47:46 +0100

    casi hecho, falta la rosbag

---
+++ Patrolling Task/goals/build/goals/CMakeFiles/version2_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/version2_publisher.dir/src/publisher.cpp.o"
+  "CMakeFiles/version2_publisher.dir/src/publisher.cpp.o.d"
+  "version2_publisher"
+  "version2_publisher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/version2_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
